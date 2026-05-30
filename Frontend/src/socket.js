cfe2d24b25a5fa14adddf562f26836b79fa5e76f
saigithubcharan <battusaicharan@gmail.com>
2026-05-30 09:10:39 +0530

    socket fix

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -3,6 +3,10 @@
 // const socket = io("http://localhost:5000");
 
 // export default socket;
+import { io } from "socket.io-client";
+
 const socket = io(
   "https://splendid-quietude-production-171f.up.railway.app"
 );
+
+export default socket;
